638fe5abc7501983c7a6020d8342b01b70acffbb
Yi Rui <yrui@sankyinc.com>
2018-07-11 10:16:05 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+.myBox {
+  border: solid 1px;
+  padding: 10px;
+  margin: 5px;
+  cursor: pointer;
+}
+.myBox:hover {
+  background: lightgray;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="synth">
+    <ul id="example-1">
+    <li v-for="note in notes" class="myBox" v-on:click="clickBox(note)">
+      {{ note }}
+    </li>
+  </ul>
+  </div>
+</template>
+
+<script>
+import Tone from 'tone'
+export default {
+  name: 'VueTone',
+  data () {
+    let synth = new Tone.MonoSynth().toMaster();
+    let notes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5'];
+    //let arr = Array.apply(null, {length: arrSize}).map(Function.call, Number).map(x => x+1);
+    return {
+      notes: notes,
+      clickBox: function(note) {
+        console.log(note);
+        synth.triggerAttackRelease(note, '8n')
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+  width: 10%;
+}
+
+a {
+  color: #35495E;
+}
+</style>
